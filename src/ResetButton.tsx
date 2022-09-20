d5aa450a6d4c683f46eab7bac644b6415289ba9f
Nina Koval <nina.koval@infopulse.com>
2022-09-20 18:45:44 +0300

    add(Button.module.css)

--- src/ResetButton.tsx
+++ src/ResetButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import styles from './Button.module.css'
 export const ResetButton = ({reset}: {reset:() => void}) => {
     return (
-        <button type="button" onClick={reset}>Reset</button>
+        <button type="button" className={styles.reset} onClick={reset}>Reset</button>
     )
 }
